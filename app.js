52916e1fe7cdacec63268cc8f6626f3f39682b49
ozansulukpinar <ozansulukpinar@hotmail.com>
2023-12-08 03:45:13 +0300

    Second commit

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var oIndex = 0;
 postQuote();
 
 function postQuote() {
-    $.getJSON(url, function(data) {
+    $.getJSON(url, function (data) {
         var quoteData = data[getRandomNumber(data.length)];
 
         writeQuote(quoteData.text);
 
         setTimeout(function () {
-            writeOwner(quoteData.author);    
+            writeOwner(quoteData.author);
         }, (quoteData.text.length * 100));
     });
 }
@@ -20,32 +20,35 @@ function getRandomNumber(limit) {
     return Math.floor(Math.random() * limit);
 }
 
-function writeQuote(quote){
+function writeQuote(quote) {
     qIndex = 0;
     write(quote, 'quote', qIndex);
 }
 
 function writeOwner(owner) {
-    if (owner == null) {
+    if (owner == null || owner == 'type.fit')
         owner = 'Anonymous';
-    }
+
+    if (owner.includes(','))
+        owner = owner.split(',')[0];
+
     oIndex = 0;
     write(owner, 'owner', oIndex);
 }
 
 function write(text, elementId, index) {
-  var element = document.getElementById('' + elementId);  
-  element.textContent = element.textContent + text.charAt(index);
-  index++;
-  
-  if(index < text.length){
-      setTimeout(function () {
-          write(text, elementId, index);
-      }, 100);
-  } 
+    var element = document.getElementById('' + elementId);
+    element.textContent = element.textContent + text.charAt(index);
+    index++;
+
+    if (index < text.length) {
+        setTimeout(function () {
+            write(text, elementId, index);
+        }, 100);
+    }
 }
 
-function changeMode() {    
+function changeMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 
